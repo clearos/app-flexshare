c5cbb2a5b07c5814f84f9763dfdb0c0b57ac92f2
pcbaldwin <pbaldwin@clearfoundation.com>
2015-01-26 16:12:31 -0500

    Clarified FollowSymlinks/Rewrite feature [tracker #2138]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'flexshare';
-$app['version'] = '2.0.5';
+$app['version'] = '2.0.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-flexshare.spec
+++ packaging/app-flexshare.spec
@@ -1,7 +1,7 @@
 
 Name: app-flexshare
 Epoch: 1
-Version: 2.0.5
+Version: 2.0.14
 Release: 1%{dist}
 Summary: Flexshare
 License: GPLv3
