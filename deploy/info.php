59b513cd89cd2a9d5c7df60afcba7a4c449690e5
pcbaldwin <pbaldwin@clearfoundation.com>
2015-03-27 14:04:39 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'flexshare';
-$app['version'] = '2.0.18';
+$app['version'] = '2.0.21';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
