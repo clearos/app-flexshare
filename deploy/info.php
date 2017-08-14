b9e05ee3c990466cc210ddccaad56b1335aefd10
pcbaldwin <peter@egloo.ca>
2017-08-14 13:55:36 -0400

    Bumped for ClearOS 7.4.0

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'flexshare';
-$app['version'] = '2.3.7';
+$app['version'] = '2.4.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
